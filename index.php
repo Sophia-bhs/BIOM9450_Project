e7f0f3c9b6ced8531815116a3edb3e265e3ef870
gazzyfire <57310458+gazzyfire@users.noreply.github.com>
2022-11-11 18:41:45 +1100

    updated code style, webpage format, and missed end </form>

--- index.php
+++ index.php
@@ -4,19 +4,29 @@
     <meta charset = "UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Practitioner Login</title>
+    <link href="login_layout.css" rel="stylesheet" type="text/css">
 </head>
+<center>
+    <h1> Medication and Diet Regime Management System </h1>
+    <body bgcolor="#E7FBFC">
+        <div>
+            <h2>
+                Login
+            </h2>
+        </div>
+        
+        <form action="login.php" method="POST">
+            <div class="container">
+                <label><b>Email</b></lable>
+                <input type="text" placeholder="Enter Email" name="email" id="email" required/>
+                <!-- <label>Name</lable> <br>
+                <input type="text" name="name" id="name" required/> <br> <br> -->
+                <label><b>Password</b></lable>
+                <input type="text" placeholder="Enter Password" name="password" id="password" required/>
 
-<h1> Medication and Diet Regime Management System </h1>
-<body>
-    <div><h2>Login</h2></div>
-    <form action="login.php" method="POST">
-        <label>Email</lable> <br>
-        <input type="text" name="email" id="email" required/> <br> <br>
-        <!-- <label>Name</lable> <br>
-        <input type="text" name="name" id="name" required/> <br> <br> -->
-        <label>Password</lable> <br>
-        <input type="text" name="password" id="password" required/> <br> <br>
-
-        <input type="submit" name="submit" id="submit"/>
-</body>
+                <button type="submit" name="submit" id="submit"/>Login</button>
+            </div>
+        </form>
+    </body>
+</center>
 </html>
