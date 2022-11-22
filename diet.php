4f199088518009cc78b94e585aa60582419c101c
Sixiao Li <sophia3lsx@gmail.com>
2022-11-22 16:55:59 +1100

    Fetch patient, diet, med lists from database

--- diet.php
+++ diet.php
@@ -4,11 +4,38 @@
     <meta charset = "UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Diet</title>
+    <link href="lists.css" rel="stylesheet" type="text/css">
 </head>
 
-<h1> Diet </h1>
+<h1> Diet list </h1>
 <body>
-    <p>Please pick a date: </p>
+<div id="wrap_list">
+        <?php
+            $conn = odbc_connect('z5262083','' ,'' ,SQL_CUR_USE_ODBC); 
+            if (!$conn) {
+                odbc_close($conn);
+                exit("Connection Failed: ".odbc_errormsg());
+            }
+            
+            $sql = "SELECT ID, DietName FROM Diet";
+            $rs  = odbc_exec($conn,$sql);  
+
+            echo "<table border-collapse: collapse>
+            <tr>
+            <th>ID</th>
+            <th>Diet</th>
+            </tr>";
+            while($row = odbc_fetch_array($rs)) {
+                echo "<tr>";
+                echo "<td>" . $row['ID']. "</td>";
+                echo "<td>" . $row['DietName'] . "</td>";
+                echo "</tr>";
+            }
+            echo "</table>";
+
+            odbc_close($conn);
+        ?>
+    </div>
     
 </body>
 </html>
--- medication.php
+++ medication.php
@@ -4,11 +4,37 @@
     <meta charset = "UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Medication</title>
+    <link href="lists.css" rel="stylesheet" type="text/css">
 </head>
 
-<h1> Medication </h1>
+<h1> Medication list</h1>
 <body>
-    <p>Please pick a date: </p>
-    
+    <div id="wrap_list">
+        <?php
+            $conn = odbc_connect('z5262083','' ,'' ,SQL_CUR_USE_ODBC); 
+            if (!$conn) {
+                odbc_close($conn);
+                exit("Connection Failed: ".odbc_errormsg());
+            }
+            
+            $sql = "SELECT ID, MedName FROM Medication";
+            $rs  = odbc_exec($conn,$sql);  
+
+            echo "<table border-collapse: collapse>
+            <tr>
+            <th>ID</th>
+            <th>Medication Name</th>
+            </tr>";
+            while($row = odbc_fetch_array($rs)) {
+                echo "<tr>";
+                echo "<td>" . $row['ID']. "</td>";
+                echo "<td>" . $row['MedName'] . "</td>";
+                echo "</tr>";
+            }
+            echo "</table>";
+
+            odbc_close($conn);
+        ?>
+    </div>
 </body>
 </html>
