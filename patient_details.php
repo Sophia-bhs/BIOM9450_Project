2d80021b080ab6268fa9240199878ba2cf33d8f1
Sixiao Li <sophia3lsx@gmail.com>
2022-11-22 17:07:14 +1100

    Patient details displayed (in a very ugly way)

--- patient_details.php
+++ patient_details.php
@@ -3,32 +3,41 @@
 <head>
     <meta charset = "UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Patients</title>
-    <link href="lists.css" rel="stylesheet" type="text/css">
+    <title>Patients Details</title>
+    <!-- <link href="lists.css" rel="stylesheet" type="text/css"> -->
 </head>
 
-<h1> Patients </h1>
+<h1> Patients Details</h1>
 <body>
 <div id="wrap_list">
         <?php
+            // ID of Patient selected
+            $id = (int) $_GET['id'];
+            
             $conn = odbc_connect('z5262083','' ,'' ,SQL_CUR_USE_ODBC); 
             if (!$conn) {
                 odbc_close($conn);
                 exit("Connection Failed: ".odbc_errormsg());
             }
-            
-            $sql = "SELECT ID, PatientName FROM Patient";
+            echo odbc_errormsg($conn);
+            $sql = "SELECT * FROM Patient where ID = $id";
             $rs  = odbc_exec($conn,$sql);  
-
+            echo odbc_errormsg($conn);
             echo "<table border-collapse: collapse>
             <tr>
             <th>ID</th>
             <th>Patient Name</th>
+            <th>Age</th>
+            <th>Gender</th>
+            <th>DOB</th>
             </tr>";
             while($row = odbc_fetch_array($rs)) {
                 echo "<tr>";
                 echo "<td>" . $row['ID']. "</td>";
-                echo "<td>" . '<a href="patient_details.php?id='.$row['ID'].'">'.$row['PatientName'].'</a>' . "</td>";
+                echo "<td>" . $row['PatientName']. "</td>";
+                echo "<td>" . $row['Age']. "</td>";
+                echo "<td>" . $row['Gender']. "</td>";
+                echo "<td>" . $row['DOB']. "</td>";
                 echo "</tr>";
             }
             echo "</table>";
