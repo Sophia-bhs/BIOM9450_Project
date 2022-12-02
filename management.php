064d6d682238f4206e9f3b13a34f362cd2c25302
Zenan <teeheezen@gmail.com>
2022-12-02 12:53:04 +1100

    Added room number and included the push to the access

--- management.php
+++ management.php
@@ -10,8 +10,8 @@
 <body>
 <?php  
 // define variables to empty values
-$nameErr = $dobErr = $genderErr = "";
-$name = $dob = $gender = "";  
+$nameErr = $dobErr = $genderErr = $roomErr = "";
+$name = $dob = $gender = $room ="";  
 
 //Input fields validation  
 if ($_SERVER["REQUEST_METHOD"] == "POST") {  
@@ -36,6 +36,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 $dobErr = "The date of birth is not a valid date in the format DD/MM/YYYY";  
             } else if (!checkdate($parts[2],$parts[1],$parts[3])){
                 $dobErr = 'The date of birth is invalid. Please check that the month is between 1 and 12, and the day is valid for that month.';
+            } else if ($parts[3] > 2022) {
+                $dobErr = 'The date of birth is invalid. Please check that the year is not greater than this year';
             }
         $DateArray = explode('/', $dob);
         $day = $DateArray[0];
@@ -51,19 +53,28 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $today = date_create($todaydate);
         $dobObj = date_create($dobFormat);
         $diff=date_diff($dobObj,$today);
-        $age_str = $diff->format("%Y");
-        $age = intval( $age_str );
+        $age = $diff->format("%Y");
         
             
             
     }
 
-    //Empty Field Validation  
+    //Gender
     if (empty($_POST["gender"])) {  
             $genderErr = "Gender is required";  
     } else {  
             $gender = input_data($_POST["gender"]);  
-    }  
+    }
+
+    // Room numver
+    if (empty($_POST["room"])) {  
+        $nameErr = "Room Number is required";  
+   } else {  
+       $room = input_data($_POST["room"]);  
+           if (!ctype_digit($room)) {  
+               $nameErr = "Only numbers are allowed";  
+           }  
+   }
 }
 function input_data($data) {  
     $data = trim($data);  
@@ -79,11 +90,11 @@ function input_data($data) {
 <br><br>  
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" >    
     Name:   
-    <input type="text" name="name">  
+    <input type="text" name="name" placeholder="Enter Name">  
     <span class="error">* <?php echo $nameErr; ?> </span>  
     <br><br>
     Date of Birth:   
-    <input type="text" name="dob">  
+    <input type="text" name="dob" placeholder="DD/MM/YYYY">  
     <span class="error">* <?php echo $dobErr; ?> </span>  
     <br><br>    
     Gender:  
@@ -92,13 +103,42 @@ function input_data($data) {
     <input type="radio" name="gender" value="other"> Other  
     <span class="error">* <?php echo $genderErr; ?> </span>  
     <br><br>
+    Room Number:   
+    <input type="text" name="room" placeholder="Enter Room Number">  
+    <span class="error">* <?php echo $roomErr; ?> </span>  
+    <br><br>
     <input type="submit" name="submit" value="Submit">   
     <br><br>                             
 </form>
   
 <?php  
     if(isset($_POST['submit'])) {  
-    if($nameErr == "" && $genderErr == "" && $dobErr == "") {  
+    if($nameErr == "" && $genderErr == "" && $dobErr == "" && $roomErr == "") {  
+
+        $conn = odbc_connect('z5256089','','',SQL_CUR_USE_ODBC);
+        
+        $sql = "SELECT * FROM Patient where PatientName = '$name' AND DOB = #$dobFormat#";
+        echo odbc_errormsg($conn);
+	    $rs = odbc_exec($conn,$sql);
+	    if (odbc_fetch_row($rs)) {
+            echo "This Patient already exists" . "<br>";
+            exit();
+	    }
+
+        $sql = "SELECT * FROM Patient where RoomNumber = $room";
+        echo odbc_errormsg($conn);
+	    $rs = odbc_exec($conn,$sql);
+	    if (odbc_fetch_row($rs)) {
+            echo "This room already has a patient" . "<br>";
+            exit();
+	    }
+
+
+        $sql = "INSERT INTO Patient (PatientName, Age, Gender, DOB, RoomNumber)
+            VALUES ('$name', '$age', '$gender', '$dobFormat', '$room')";	
+        $rs = odbc_exec($conn,$sql);
+        echo odbc_errormsg($conn);
+
         echo "<h3 color = #FF0001> <b>You have sucessfully registered.</b> </h3>";  
         echo "<h2>Your Input:</h2>";  
         echo "Name: " .$name;  
@@ -108,17 +148,11 @@ function input_data($data) {
         echo "Date of Birth : " .$dob;
         echo "<br>"; 
         echo "Age : " .$age;
-        echo gettype($name);
-        echo gettype($gender);
-        echo gettype($dobFormat);
-        echo gettype($age);
+        echo "<br>";  
+        echo "Room Number : " .$room;
+        echo "<br>";
 
 
-         
-    // $sql = "INSERT INTO Patient (PatientName, Gender, DOB)
-	//	VALUES ('$name', '$gender', '$dob')";	
-    //    $rs = odbc_exec($conn,$sql);
-    //    echo odbc_errormsg($conn);
     } else {  
         echo "<h3> <b>You didn't filled up the form correctly.</b> </h3>";  
     }  
